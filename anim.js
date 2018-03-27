dd8ac9f2b1c9c6d2d85f9667a312f0d38da2e5e8
otottkovi <otottkoviwin@gmail.com>
2018-03-27 21:01:45 +0200

    first files

---
+++ anim.js
@@ -0,0 +1,29 @@
+
+noStroke();
+
+// position of the car
+var x = 281;
+
+// draw the car body
+fill(255, 0, 115);
+rect(x, 200, 100, 20);
+rect(x + 15, 178, 70, 40);
+
+// draw the wheels
+fill(77, 66, 66);
+ellipse(x + 25, 221, 24, 24);
+ellipse(x + 75, 221, 24, 24);
+
+draw = function() {
+	background(151, 244, 247);
+    // draw the car body
+	fill(255, 0, 115);
+	rect(x, 200, 100, 20);
+	rect(x + 15, 178, 70, 40);
+
+	// draw the wheels
+	fill(77, 66, 66);
+	ellipse(x + 25, 221, 24, 24);
+	ellipse(x + 75, 221, 24, 24);
+	x = x + 1
+};
